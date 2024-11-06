46d78b4bbfbb5448bc9ad4f18d1a59e6787113dc
Ankit Gupta <ankit.gupta8@dell.com>
2024-11-06 16:32:24 +0530

    logout+password update

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -2,12 +2,44 @@ const express = require("express")
 const profileRouter = express.Router();
 
 const { userAuth } = require("../middlewares/auth");
+const { validateEditProfileData } = require("../utils/validation");
+const bcrypt = require("bcrypt");
 
-profileRouter.get("/profile", userAuth, async (req, res) => {
+profileRouter.get("/profile/view", userAuth, async (req, res) => {
     try {
       const user = req.user;
   
       res.send(user);
+    } catch (err) {
+      res.status(400).send("ERROR : " + err.message);
+    }
+  });
+
+
+  profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
+    try {
+      if (!validateEditProfileData(req)) {
+        throw new Error("Invalid Edit Request");
+      }
+      const loggedInUser = req.user;
+      Object.keys(req.body).forEach((key) => (loggedInUser[key] = req.body[key]));
+      await loggedInUser.save();
+      res.json({
+        message: `${loggedInUser.firstName}, your profile updated successfuly`,
+        data: loggedInUser,
+      });
+    } catch (err) {
+      res.status(400).send("ERROR : " + err.message);
+    }
+  });
+
+  profileRouter.patch("/profile/password", userAuth, async (req, res) => {
+    try {
+      const loggedInUser = req.user;
+      const passwordHash = await bcrypt.hash(req.body.password, 10);
+      loggedInUser.password = passwordHash;
+      await loggedInUser.save();
+      res.send("password update Successfully");
     } catch (err) {
       res.status(400).send("ERROR : " + err.message);
     }
