e9c6f1a81c29909ce2d249377b0d58c3765e8bea
Ankit Gupta <ankit.gupta8@dell.com>
2024-11-13 12:36:28 +0530

    Fix error in connection request

--- src/routes/user.js
+++ src/routes/user.js
@@ -40,15 +40,12 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
       .populate("fromUserId", USER_SAFE_DATA)
       .populate("toUserId", USER_SAFE_DATA);
 
-    console.log(connectionRequests);
-
     const data = connectionRequests.map((row) => {
       if (row.fromUserId._id.toString() === loggedInUser._id.toString()) {
         return row.toUserId;
       }
       return row.fromUserId;
     });
-
     res.json({ data });
   } catch (err) {
     res.status(400).send({ message: err.message });
